4060d36c5756be3cbd501d70ea0c1020fa5f9d5c
prana <pranaytb777@gmail.com>
2025-07-25 19:01:02 +0530

    Initialize Spring Boot application with basic configuration and structure

---
+++ FirstSpring/src/main/java/org/pranay/firstspring/FirstSpringApplication.java
@@ -0,0 +1,13 @@
+package org.pranay.firstspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringApplication.class, args);
+    }
+
+}
