273b797ab8afd1e387ae1ddcf2dbd4b001364022
prana <pranaytb777@gmail.com>
2025-07-29 22:34:15 +0530

    Refactor import statements in CategoryController for improved organization

--- sb-ecom/src/main/java/com/ecommerce/sbecom/controllers/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/controllers/CategoryController.java
@@ -1,13 +1,14 @@
 package com.ecommerce.sbecom.controllers;
 
-import com.ecommerce.sbecom.models.Category;
-import com.ecommerce.sbecom.services.CategoryService;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.ecommerce.sbecom.models.Category;
+import com.ecommerce.sbecom.services.CategoryService;
 
 @RestController
 public class CategoryController {
