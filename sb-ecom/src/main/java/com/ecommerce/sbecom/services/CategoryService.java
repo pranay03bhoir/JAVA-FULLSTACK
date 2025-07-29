be3395368e2259af73a56667ff502790b9793244
prana <pranaytb777@gmail.com>
2025-07-29 22:33:41 +0530

    Add Category management functionality with CategoryController, Category model, and CategoryService implementation

---
+++ sb-ecom/src/main/java/com/ecommerce/sbecom/services/CategoryService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.sbecom.services;
+
+import com.ecommerce.sbecom.models.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+
+    List<Category> getAllCategories();
+
+    void createCategory(Category category);
+}
