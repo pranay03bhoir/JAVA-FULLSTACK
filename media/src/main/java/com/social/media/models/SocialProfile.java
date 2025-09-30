d300a73df9b99fcde14126c5d1f411ed022a8ae9
prana <pranaytb777@gmail.com>
2025-09-30 17:37:58 +0530

    Add initial implementation of social media application; create models, repositories, services, and controllers for users, groups, profiles, and posts; include data initialization logic

---
+++ media/src/main/java/com/social/media/models/SocialProfile.java
@@ -0,0 +1,20 @@
+package com.social.media.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SocialProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "social_user")
+    private SocialUser user;
+}
