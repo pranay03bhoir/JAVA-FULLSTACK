552a06c4e629f7ed3701296c8d39b4de7af1db96
prana <pranaytb777@gmail.com>
2025-10-14 13:34:26 +0530

    First commit

---
+++ securitydemo/src/main/java/com/example/securitydemo/GreetingsController.java
@@ -0,0 +1,16 @@
+package com.example.securitydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+	
+	@GetMapping("/hello")
+	public String greetings() {
+		return "Hello";
+	}
+	
+
+}
+ 
