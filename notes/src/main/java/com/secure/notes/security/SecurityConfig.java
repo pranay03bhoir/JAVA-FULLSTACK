63e27bf805b35f619550d7a9d3607d0852926ff3
prana <pranaytb777@gmail.com>
2025-11-03 15:55:52 +0530

    Tested out the created API endpoints in postman

--- notes/src/main/java/com/secure/notes/security/SecurityConfig.java
+++ notes/src/main/java/com/secure/notes/security/SecurityConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,14 +22,27 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests
+                .anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
+       // http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
+
+/*
+*     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/contact").permitAll()
                 .requestMatchers("/public/**").permitAll() // publicly available endpoints.
                 .requestMatchers("/admin").denyAll() // this is used to deny any connection or access to the specific endpoint in our case "/admin".
                 .anyRequest().authenticated());
+        http.csrf(AbstractHttpConfigurer::disable);
         http.sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
        // http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
-}
+*
+* */
