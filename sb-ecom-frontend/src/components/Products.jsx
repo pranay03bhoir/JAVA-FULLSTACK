bba24e19062682e9bc037a829ad276765a60a0af
prana <pranaytb777@gmail.com>
2026-02-24 11:43:17 +0000

    Configured react for the sb-ecom full-stack application and added folder structure to the project.

---
+++ sb-ecom-frontend/src/components/Products.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Products = () => {
+  const isLoading = false;
+  const errorMessage = "";
+  const products = [
+    {
+      productId: 652,
+      productName: "Iphone Xs max",
+      image: "https://placehold.co/600x400",
+      description:
+        "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
+      quantity: 0,
+      price: 1450.0,
+      discount: 10.0,
+      specialPrice: 1305.0,
+    },
+    {
+      productId: 654,
+      productName: "MacBook Air M2s",
+      image: "https://placehold.co/600x400",
+      description:
+        "Ultra-thin laptop with Apple's M2 chip, providing fast performance in a lightweight, portable design.",
+      quantity: 0,
+      price: 2550.0,
+      discount: 20.0,
+      specialPrice: 2040.0,
+    },
+  ];
+  return (
+    <div>
+      <h1>Products</h1>
+    </div>
+  );
+};
+
+export default Products;
