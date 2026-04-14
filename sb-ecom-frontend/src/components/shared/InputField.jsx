52a30b5febd3703281fe45ac34df5151a5e771e6
pranaybhoir <pranaytb777@gmail.com>
2026-04-14 18:47:00 +0530

    - Add `authReducer` for managing user authentication state in Redux store - Integrate `authReducer` into Redux store configuration - Remove unnecessary `peer` entries from `package-lock.json` - Update `application.properties` to standardize sensitive data handling - Add placeholder `InputField.jsx` component for future UI usage improvements

---
+++ sb-ecom-frontend/src/components/shared/InputField.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const InputField = (
+  label,
+  id,
+  type,
+  errors,
+  register,
+  required,
+  message,
+  min,
+  value,
+  placeholder,
+) => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default InputField;
--- sb-ecom-frontend/src/store/reducers/store.js
+++ sb-ecom-frontend/src/store/reducers/store.js
@@ -2,18 +2,24 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./productReducer.js";
 import { errorReducer } from "./errorReducer.js";
 import { cartReducer } from "./cartReducer.js";
+import {authReducer} from "./authReducer.js";
+
+const user = localStorage.getItem("auth")
+    ? JSON.parse(localStorage.getItem("auth"))
+    : [];
 
 const cartItems = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
-const initialState = { carts: { cart: cartItems } };
+const initialState = {auth:{user:user} ,carts: { cart: cartItems } };
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
     errors: errorReducer,
     carts: cartReducer,
+    auth:authReducer,
   },
   preloadedState: initialState,
 });
